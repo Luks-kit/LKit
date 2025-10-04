113272be10b2c98319aece2f06f3daf59ac403c0
Luks-kit <lushdudeaimboot@gmail.com>
2025-10-03 23:33:58 -0500

    Update parser.hpp

--- inc/parser.hpp
+++ inc/parser.hpp
@@ -1,10 +1,26 @@
-#ifndef PARSER_HPP
-#define PARSER_HPP
-
+// parser.hpp
+#pragma once
+#include "expr.hpp"
+#include "stmt.hpp"
+#include "decl.hpp"
 #include "lexer.hpp"
-#include "ast.hpp"
+#include <memory>
 
-AST* parse(Lexer& lex);  // entry point
+class Parser {
+public:
+    explicit Parser(Lexer& l) : lexer(l) { advance(); }
 
-#endif
+    std::unique_ptr<Node> parse();
 
+private:
+    Lexer& lexer;
+    Token current;
+
+    void advance() { current = lexer.get_next_token(); }
+    void expect(TokenType type);
+
+    std::unique_ptr<Expr> parse_expr();
+    std::unique_ptr<Stmt> parse_stmt();
+    std::unique_ptr<Decl> parse_decl();
+};
+AST* parse(Lexer& lex); // old entry point
