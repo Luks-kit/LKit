bb0e2e771bcbec6a362a495d94c0e4bbcee9dfe5
Luks-kit <lushdudeaimboot@gmail.com>
2025-10-03 23:31:00 -0500

    Create stmt.hpp

---
+++ inc/stmt.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "node.hpp"
+#include "expr.hpp"
+#include <vector>
+#include <memory>
+
+enum class StmtKind { ExprStmt, Block, If, While, Return };
+
+struct Stmt : Node {
+    StmtKind kind;
+    explicit Stmt(StmtKind k) : kind(k), nodeType(NodeType::Stmt) {}
+    virtual ~Stmt() = default;
+};
+
+struct ExprStmt : Stmt {
+    std::unique_ptr<Expr> expr;
+    ExprStmt(std::unique_ptr<Expr> e) 
+        : Stmt(StmtKind::ExprStmt), expr(std::move(e)) {}
+};
+
+struct BlockStmt : Stmt {
+    std::vector<std::unique_ptr<Stmt>> stmts;
+    BlockStmt() : Stmt(StmtKind::Block) {}
+};
+
+struct IfStmt : Stmt {
+    std::unique_ptr<Expr> cond;
+    std::unique_ptr<Stmt> then_branch, else_branch;
+    IfStmt(std::unique_ptr<Expr> c, std::unique_ptr<Stmt> t, std::unique_ptr<Stmt> e)
+        : Stmt(StmtKind::If), cond(std::move(c)), then_branch(std::move(t)), else_branch(std::move(e)) {}
+};
+
+struct WhileStmt : Stmt {
+    std::unique_ptr<Expr> cond;
+    std::unique_ptr<Stmt> body;
+    WhileStmt(std::unique_ptr<Expr> c, std::unique_ptr<Stmt> b)
+        : Stmt(StmtKind::While), cond(std::move(c)), body(std::move(b)) {}
+};
